2040ec82e38c04bdc27f3f21d904ce697e26963a
Jenny Leung <jenny@jenny-leung.com>
2017-10-01 17:23:01 -0700

    Day 10 - Ravenous II

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,8 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        { this.props.businesses.map(business => <Business business={ business } />
+        ) }
       </div>
     );
   }
