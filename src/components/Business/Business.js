73c485d8798d0ec962447cd12c74e5644cbd976f
Jenny Leung <jenny@jenny-leung.com>
2017-10-16 13:32:49 -0700

    update Business style

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -16,7 +16,7 @@ class Business extends React.Component {
             <p>{ this.props.business.state + ' ' + this.props.business.zipCode }</p>
           </div>
           <div className="Business-reviews">
-            <h3>{ this.props.business.category }</h3>
+            <h3>{ this.props.business.category.toUpperCase() }</h3>
             <h3 className="rating">{ this.props.business.rating } stars</h3>
             <p>{ this.props.business.reviewCount } reviews</p>
           </div>
